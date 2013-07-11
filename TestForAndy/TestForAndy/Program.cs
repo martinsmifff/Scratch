40c07fea5ee95f90d1981293037e6579114aaa10
Smifffy <martin.smith@blueyonder.co.uk>
2013-07-11 15:43:58 +0100

    added a new scratch project to demo something to andy

---
+++ TestForAndy/TestForAndy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestForAndy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load("C:\\Andy.xml");
+            XmlNode n = xml.SelectSingleNode("//OtherInfo");
+
+            string t = n.InnerText;
+            Console.WriteLine(t);
+
+            t = t.Replace("<br />", "~").Replace("<br/>", "~");
+
+            string[] pairs = t.Split('~');
+
+            foreach (string p in pairs)
+            {
+                string[] s = p.Split('|');
+
+                Console.WriteLine("{0} - {1}", s[0], s[1]);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
